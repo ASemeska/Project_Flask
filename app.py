bdf4f1d6aa0ae9a7cd37505de661899129fabf68
ASemeska <adomassem@gmail.com>
2022-10-25 17:04:37 +0300

    API added

--- app.py
+++ app.py
@@ -1,31 +1,35 @@
+
 from flask import Flask, flash, redirect, render_template, request, session, url_for
-
-
+import requests
+import json
 
 app = Flask(__name__)
 app.secret_key = "zZdASfFReTRT8"
 
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def home():
-    return render_template("index.html")
+    req = requests.get('http://www.boredapi.com/api/activity/')
+    data = json.loads(req.text)
+    print(data['key'])
+    return render_template("index.html", data = data)
 
-@app.route("/login", methods = ["POST", "GET"])
-def login():
-    if request.method == "POST":
-        user = request.form["nm"]
-        session["user"] = user
-        return redirect(url_for("user"))
-    else:
-        return render_template("login.html")
+# @app.route("/login", methods = ["POST", "GET"])
+# def login():
+#     if request.method == "POST":
+#         user = request.form["nm"]
+#         session["user"] = user
+#         return redirect(url_for("user"))
+#     else:
+#         return render_template("login.html")
 
-@app.route("/user")
-def user():
-    if "user" in session:
-        user = session["user"]
-        return render_template("user.html")
-    else:
-        return redirect(url_for("login"))
+# @app.route("/user")
+# def user():
+#     if "user" in session:
+#         user = session["user"]
+#         return render_template("user.html")
+#     else:
+#         return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
